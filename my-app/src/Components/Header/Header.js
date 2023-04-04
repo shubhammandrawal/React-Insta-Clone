66b1af6c3af6637b22b04367e503d4a2cf3df75f
shubhammandrawal <Shubham@7785>
2023-04-04 20:21:45 +0530

    adding Landing and Post page

---
+++ my-app/src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import circle from "../images/icon.svg";
+import camera from "../images/camera.png";
+import './header.css'
+
+
+export default function AppHeader(){
+    return<>
+        <navbar className="top-header">
+            <div className="circle">
+                <img src={circle} alt="circle-icon" />
+                <p>INSTACLONE</p>
+            </div>
+            <div className="camera">
+                <img src={camera} alt="camera-icon" />
+            </div>
+        </navbar>
+    </>
+}
--- my-app/src/Components/LandingComponentFolder/Landing.js
+++ my-app/src/Components/LandingComponentFolder/Landing.js
@@ -2,10 +2,12 @@ import React from "react";
 import home from "../../images/home.png"
 import {Link} from 'react-router-dom';
 import './Landing.css';
+import AppHeader from "../Header/Header";
 
 
 export default function LandingPage(){
     return<>
+    <AppHeader />
         <div className="home-content">
             <div className="landing-img">
                 <img src={home} alt="imga" />
